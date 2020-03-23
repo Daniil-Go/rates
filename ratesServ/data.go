3c0e765b985dc2eb91949a9dffa4cc7d0729264e
Daniil <97057@mail.ru>
2020-03-23 16:42:48 +0300

    exchange implementation

--- ratesServ/data.go
+++ ratesServ/data.go
@@ -3,20 +3,22 @@ package main
 import "sync"
 
 type Cache struct {
-	Rates map[string]float64 `json:"rates,omitempty"`
+	Rates map[string]float64 `json:"rates"`
 	Base  string             `json:"base"`
 	Date  string             `json:"date"`
 	mu    sync.Mutex
 }
 
 type Response struct {
-	Base  string  `json:"base,omitempty"`
-	Quote string  `json:"quote,omitempty"`
-	Rate  float64 `json:"rate,omitempty"`
-	Error string  `json:"error,omitempty"`
+	BaseName  string
+	BaseRate  float64
+	QuoteName string
+	QuoteRate float64
+	Rate      float64
+	Error     string
 }
 
 type SearchParams struct {
-	Base  string `json:"base"`
-	Quote string `json:"quote"`
+	Base  string
+	Quote string
 }
